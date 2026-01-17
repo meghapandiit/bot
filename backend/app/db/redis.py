9970604e085e32e370a271b2fc7318b7d66c7f1d
meghapandiit <153391690+meghapandiit@users.noreply.github.com>
2026-01-17 23:26:58 +0530

    disable redis fallback

--- backend/app/db/redis.py
+++ backend/app/db/redis.py
@@ -1,12 +1,25 @@
-import redis
-import json
-from app.core.config import REDIS_URL
+import os
 
-r = redis.from_url(REDIS_URL, decode_responses=True)
+REDIS_URL = os.getenv("REDIS_URL")
 
-def get_session(session_id: str):
+# fallback memory
+_memory = {}
+
+def get_session(session_id: str) -> dict:
+    if not REDIS_URL:
+        return _memory.get(session_id, {})
+
+    import redis
+    r = redis.from_url(REDIS_URL, decode_responses=True)
     data = r.get(session_id)
-    return json.loads(data) if data else {}
+    return eval(data) if data else {}
 
-def set_session(session_id: str, data: dict):
-    r.set(session_id, json.dumps(data), ex=60*60*24)
+
+def set_session(session_id: str, session: dict):
+    if not REDIS_URL:
+        _memory[session_id] = session
+        return
+
+    import redis
+    r = redis.from_url(REDIS_URL, decode_responses=True)
+    r.set(session_id, str(session))
